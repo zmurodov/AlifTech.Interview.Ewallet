ce795e373803ae06193fd8003cf5f4d5965ee883
zufar <zufar4554murodov@gmail.com>
2023-05-14 20:40:14 +0500

    fixed seed data migration downgrade sql

--- AlifTech.Interview.Ewallet/Data/Scripts/M0005_Seed_Data_Down.sql
+++ AlifTech.Interview.Ewallet/Data/Scripts/M0005_Seed_Data_Down.sql
@@ -1,5 +1,8 @@
 delete from wallets where id = 'wallet-1';
 delete from wallets where id = 'wallet-2';
 
+delete from wallet_types where id = 1;
+delete from wallet_types where id = 2;
+
 delete from users where id = 'user-1';
 delete from users where id = 'user-2';
